91f5482f24c3c2d6070547db0020553322f91219
Marcos Freiberger <marcos.freiberger@bradesco.com.br>
2023-01-24 14:10:14 -0300

    Nested routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Navbar from './components/Navbar';
 import Product from './pages/Product';
+import Info from './pages/Info';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/products/:id' element={<Product />} />
+          <Route path='/products/:id/info' element={<Info />} />
         </Routes>
       </BrowserRouter>
     </div>
