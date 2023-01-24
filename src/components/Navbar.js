8cf2931d763c04104b0543c1fa5cb004c7581288
Marcos Freiberger <marcos.freiberger@bradesco.com.br>
2023-01-24 14:27:23 -0300

    Link ativo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
     return (
         <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>Sobre</Link>
+            {/* <Link to='/'>Home</Link>
+            <Link to='/about'>Sobre</Link> */}
+            <NavLink to='/'
+            // className={({ isActive }) => (isActive ? 'esta-ativo' : 'não-ativo')}
+            >Home</NavLink>
+            <NavLink to='/about'>Sobre</NavLink>
         </nav>
     )
 }
